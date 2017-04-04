734471c67eff6461596e5202c176708ca6600a7f
Eva Balog <evebalog@netscape.net>
2017-04-04 16:50:22 +0100

    Added view for creating restaurant, feature test still WIP

--- yelp_clone/spec/features/restaurants_features_spec.rb
+++ yelp_clone/spec/features/restaurants_features_spec.rb
@@ -4,7 +4,7 @@ feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
-      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_content 'No restaurants have been listed yet'
       expect(page).to have_link 'Add a restaurant'
     end
   end
@@ -17,7 +17,18 @@ feature 'restaurants' do
     scenario 'display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('Pret')
-      expect(page).not_to have_content('No restaurants yet')
+      expect(page).not_to have_content('No restaurants have been listed yet')
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Pret'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Pret'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
