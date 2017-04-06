7b4d951ff315a46df8a21f72cc44c926e35b842b
Eva Balog <evebalog@netscape.net>
2017-04-06 12:03:51 +0100

    Restaurant can be deleted

--- yelp_clone/spec/features/restaurants_features_spec.rb
+++ yelp_clone/spec/features/restaurants_features_spec.rb
@@ -88,4 +88,16 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants/1'
     end
   end
+
+  context 'deleting restaurants' do
+
+    before { Restaurant.create name: 'Cafe', description: 'Unique' }
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete Cafe'
+      expect(page).not_to have_content 'Cafe'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
