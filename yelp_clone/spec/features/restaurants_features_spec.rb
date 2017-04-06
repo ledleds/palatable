638f64c44dab1f5c58b80f605d790a7e362b2db9
Eva Balog <evebalog@netscape.net>
2017-04-06 11:00:22 +0100

    Feature tests for average rating passed

--- yelp_clone/spec/features/restaurants_features_spec.rb
+++ yelp_clone/spec/features/restaurants_features_spec.rb
@@ -32,15 +32,15 @@ feature 'restaurants' do
     end
 
     context 'an invalid restaurant' do
-     scenario 'does not let you submit a name that is too short' do
-       visit '/restaurants'
-       click_link 'Add a restaurant'
-       fill_in 'Name', with: 'Pr'
-       click_button 'Create Restaurant'
-       expect(page).not_to have_css 'h2', text: 'Pr'
-       expect(page).to have_content 'error'
-     end
-   end
+      scenario 'does not let you submit a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'Pr'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'Pr'
+        expect(page).to have_content 'error'
+      end
+    end
   end
 
   context 'viewing restaurants' do
@@ -48,10 +48,27 @@ feature 'restaurants' do
     let!(:pret){ Restaurant.create(name: 'Pret') }
 
     scenario 'lets a user view a restaurant' do
-     visit '/restaurants'
-     click_link 'Pret'
-     expect(page).to have_content 'Pret'
-     expect(current_path).to eq "/restaurants/#{pret.id}"
+      visit '/restaurants'
+      click_link 'Pret'
+      expect(page).to have_content 'Pret'
+      expect(current_path).to eq "/restaurants/#{pret.id}"
+    end
+
+    scenario 'average rating shows on index page and restaurant show page' do
+      visit '/restaurants'
+      click_link 'Pret'
+      click_link 'Review Pret'
+      fill_in 'Comment', with: 'Very nice'
+      select '4', from: 'Rating'
+      click_button 'Submit Review'
+      expect(page).to have_content "4"
+      click_link 'Review Pret'
+      fill_in 'Comment', with: 'Super'
+      select '5', from: 'Rating'
+      click_button 'Submit Review'
+      expect(page).to have_content "4.5"
+      visit '/restaurants'
+      expect(page).to have_content "4.5"
     end
   end
 end
