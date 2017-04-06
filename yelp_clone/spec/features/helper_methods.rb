498cc467a2abd0834b0e82df60e08b7a3961049f
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-06 16:18:03 +0100

    Merged helper methods

--- yelp_clone/spec/features/helper_methods.rb
+++ yelp_clone/spec/features/helper_methods.rb
@@ -1,3 +1,20 @@
+def sign_up
+  visit('/')
+  click_link('Sign up')
+  fill_in('Email', with: 'test@example.com')
+  fill_in('Password', with: 'testtest')
+  fill_in('Password confirmation', with: 'testtest')
+  click_button('Sign up')
+end
+
+def sign_in
+  visit('/')
+  click_link('Sign in')
+  fill_in('Email', with: 'test@example.com')
+  fill_in('Password', with: 'testtest')
+  click_button('Log in')
+end
+
 def create_new_restaurant_jasmine
   visit '/restaurants'
   click_link 'Add a restaurant'
