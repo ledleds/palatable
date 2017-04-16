16cfd2a71a4e8673bdb2a9a2235a30031fe79986
Vicky Ledsom <victorialedsom@gmail.com>
2017-04-16 21:25:43 +0100

    Edit styling and add screenshots of project

--- yelp_clone/app/helpers/reviews_helper.rb
+++ yelp_clone/app/helpers/reviews_helper.rb
@@ -1,6 +1,6 @@
 module ReviewsHelper
   def star_rating(rating)
-    return rating unless rating.respond_to?(:round)
+    return "No reviews yet" unless rating.respond_to?(:round)
     remainder = 5 - rating.round
     "★" * rating.round + "☆" * remainder
   end
--- yelp_clone/spec/helpers/reviews_helper_spec.rb
+++ yelp_clone/spec/helpers/reviews_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ReviewsHelper, :type => :helper do
   context '#star_rating' do
     it 'does nothing for not a number' do
-      expect(helper.star_rating('N/A')).to eq 'N/A'
+      expect(helper.star_rating(nil)).to eq 'No reviews yet'
     end
   end
 
